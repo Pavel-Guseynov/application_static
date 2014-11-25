e222361d2b45ea1d36e61e8ff576ce1e5b738488
allnightlong <allnightlong@allnightlong.ru>
2014-11-26 01:49:03 +0300

    long_description

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer "allnightlong"
 maintainer_email "allnightlong@allnightlong.ru"
 license "Apache 2.0"
 description "Installs/Configures application_static"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 version "0.1.0"
 
