4efadd0cb2d5cb5f28843921840d1646f6725395
allnightlong <allnightlong@allnightlong.ru>
2014-11-26 05:01:20 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name "application_static"
 maintainer "allnightlong"
 maintainer_email "allnightlong@allnightlong.ru"
 license "Apache 2.0"
-description "Installs/Configures application_static"
+description "Simple chef cookbook to quick host static web sites with nginx."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
 version "0.3.0"
